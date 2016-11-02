a5fa11ba480e36521a267bfc3de93ce82873a6da
Kinga Magyar <kinga.magyar89@gmail.com>
2016-11-02 11:35:55 +0100

    Ex04 - corrected to return

--- 04.cpp
+++ 04.cpp
@@ -6,22 +6,21 @@ using namespace std;
  * Create a function that takes an integer and returns how many divisors it has
  */
 
-void num_of_divisors(int num) {
+int num_of_divisors(int num) {
   int divisor = 0;
   for (int i = 1; i <= num; i++) {
     if (num % i == 0) {
       divisor++;
     }
   }
-  cout << divisor;
+  return divisor;
 }
 
 int main() {
   int input;
   cout << "Please give me an integer and I'll show you how many divisors it has: ";
   cin >> input;
-  cout << "Number " << input << " has this many divisors: ";
-  num_of_divisors(input);
+  cout << "Number " << input << " has this many divisors: " << num_of_divisors(input);
   return 0;
 }
 
