284c609a1e934111d6ebb3368e4ced9de35b2523
Kinga Magyar <kinga.magyar89@gmail.com>
2016-11-02 11:06:15 +0100

    Exam exercises 1-4 corrections

--- 01.cpp
+++ 01.cpp
@@ -6,15 +6,17 @@ using namespace std;
  * Create a function that decides if there is a bigger or equal number than a given number in an array
  * It should take the array, it's length and a number as a parameter, then return a boolean
  */
-bool decide(int* array, int length, int num) {
+bool decide(int array[], int length, int num) {
+  bool measure = 1;
   for (int i = 0; i < length; i++) {
     if (array[i] >= num) {
-	  return true;
-	} else {
-	  return false;
-	}
-    return array;
+      measure = 1;
+      break;
+    } else {
+      measure = 0;
+    }
   }
+  return measure;
 }
 
 int main() {
@@ -22,7 +24,7 @@ int main() {
   int input;
   cout << "Please give me a number: ";
   cin >> input;
-  cout << "Is there a bigger or equal number to " << input << " in the array? (Y=1 / N=0) " << decide(array, 5, input) << endl;
+  cout << "Is there a bigger or equal number to " << input << " in the array? (Y=1 / N=0): " << decide(array, 5, input) << endl;
   return 0;
 }
 
